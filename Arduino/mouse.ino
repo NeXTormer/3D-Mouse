56dbd8419301af8cab680bc1c58065fc08c9235c
Felix Holz <felix.holz@me.com>
2018-05-08 08:49:01 +0200

    arduino

--- Arduino/mouse.ino
+++ Arduino/mouse.ino
@@ -2,8 +2,18 @@
 #include "SparkFunLSM6DS3.h"
 #include "Wire.h"
 #include "SPI.h"
+#include <ESP8266WiFi.h>
+#include <WiFiUdp.h>
 
-LSM6DS3 myIMU(0x6A); //Default constructor is I2C, addr 0x6B
+LSM6DS3 myIMU(0x6A);
+
+
+const char* ssid = "htl-IoT";
+const char* password = "iot..2015";
+
+WiFiUDP Udp;
+unsigned int port = 8888;
+char incomingPacket[255];
 
 void setup() 
 {
@@ -14,14 +24,25 @@ void setup()
   
   //Call .begin() to configure the IMU
   myIMU.begin();
+
+
+  Serial.printf("Connecting to %s ", ssid);
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println(" connected");
+
+  Udp.begin(localUdpPort);
+  Serial.printf("UDP Server Started");
   
 }
 
 
 void loop()
 {
-  //Get all parameters
-
   Serial.println(millis());
   
   Serial.print("\nAccelerometer:\n");
@@ -47,5 +68,25 @@ void loop()
   Serial.println(myIMU.readTempF(), 4);
 
   delay(100);
+
+  /*
+  int packetSize = Udp.parsePacket();
+  if (packetSize)
+  {
+    // receive incoming UDP packets
+    Serial.printf("Received %d bytes from %s, port %d\n", packetSize, Udp.remoteIP().toString().c_str(), Udp.remotePort());
+    int len = Udp.read(incomingPacket, 255);
+    if (len > 0)
+    {
+      incomingPacket[len] = 0;
+    }
+    Serial.printf("UDP packet contents: %s\n", incomingPacket);
+    */
+
+    // send back a reply, to the IP address and port we got the packet from
+    Udp.beginPacket(Udp.remoteIP(), Udp.remotePort());
+    Udp.write(replyPacket);
+    Udp.endPacket();
+  }
 
 }
